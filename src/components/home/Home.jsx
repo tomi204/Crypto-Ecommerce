98c4bbf0fd65e2838140a7480f938a8dbe8df3a6
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-07-30 19:27:37 -0300

    cart

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Contador from "../../common/cart-items/Cart";
 import "./Home.css";
 import SlideCard from "./ItemList";
 function Home() {
   return (
     <section className="home">
+      <Contador />
       <SlideCard />
     </section>
   );
---
+++ src/components/productos/descuento.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const descuento = () => {
+  return (
+
+  )
+}
+
+export default descuento;

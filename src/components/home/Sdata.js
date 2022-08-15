9af1b6df44a7e3ffe72d4a6f571cf42202a8afc9
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-08-15 18:42:55 -0300

    arreglando img

--- src/components/home/Sdata.js
+++ src/components/home/Sdata.js
@@ -3,28 +3,32 @@ const Sdata = [
     id: 1,
     title: "PC GAMING AL MEJOR PRECIO",
     desc: "I7 11700K + Placa de video 8GB RTX 3070",
-    cover: "./images/SlideCard/slide-1.png",
+    cover:
+      "https://cdn.coordiutil.com/imagen-pc_gamer_ryzen_5_5600g_graficos_vega_board_b550_ram_8_gigas_m2_250_fuente_real-2182071-800-600-1-75.jpg",
     stock: 5,
   },
   {
     id: 2,
     title: "Las Mejores Placas De Video",
     desc: "LLEVATE UNA DE LAS MEJORES PLACAS DE VIDEO",
-    cover: "./images/SlideCard/slide-2.png",
+    cover:
+      "https://fhgamer.ar/wp-content/uploads/2020/10/turbo2V1-1024x695.png",
     stock: 5,
   },
   {
     id: 3,
     title: "Notebooks Gamer",
     desc: "La Mejor Notebook Gamer con un I7 y una 3060TI",
-    cover: "./images/SlideCard/slide-3.png",
+    cover:
+      "https://ar-media.hptiendaenlinea.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/C/4C7S3LA-1_T1651684547.png",
     stock: 5,
   },
   {
     id: 4,
     title: "MONITORES GAMER",
     desc: "Monitores 144hz y 240hz",
-    cover: "./images/SlideCard/slide-4.png",
+    cover:
+      "https://images.samsung.com/is/image/samsung/latin-curved-gaming-c27rg50fql-lc27rg50fqlxzp-frontblack-207663455?$650_519_PNG$",
     stock: 5,
   },
 ];

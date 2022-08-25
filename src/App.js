8d4777c2b36ef2e419973a9b4780c7534bb6a602
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-08-25 16:16:03 -0300

    firebase y use effect

--- src/App.js
+++ src/App.js
@@ -6,51 +6,14 @@ import {
   BrowserRouter,
   Routes,
 } from "react-router-dom";
-import { useEffect } from "react";
 import Navbar from "./components/Header/Navbar";
 import NavL from "./components/Lateralnav/NavL";
 import Productos from "./components/Productos/Productos";
 import Cart from "./components/CartItems/Cart";
 import ProductDetails from "./components/Productos/ProductDetails";
 import CartContextProvider from "./components/CartItems/CartContext";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDmxCV8XAUCNRwY0bDJpKdN9zRUmtMTyBw",
-  authDomain: "proyectoreactch-29d90.firebaseapp.com",
-  databaseURL: "https://proyectoreactch-29d90-default-rtdb.firebaseio.com",
-  projectId: "proyectoreactch-29d90",
-  storageBucket: "proyectoreactch-29d90.appspot.com",
-  messagingSenderId: "946188183402",
-  appId: "1:946188183402:web:466093a53c63b64c60e713",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase
-// export const DB = getFirestore(app);
 
 function App() {
-  useEffect(() => {
-    const app = initializeApp(firebaseConfig);
-    const DB = getFirestore(app);
-    const colRef = collection(DB, "products");
-
-    getDocs(colRef).then(
-      (snapshot) => {
-        console.log(">> snapshot.docs", snapshot.docs);
-      },
-      (error) => {
-        console.log("error");
-      }
-    );
-  });
   return (
     <div className="App">
       <BrowserRouter>
--- src/components/Api/Firebase.js
+++ src/components/Api/Firebase.js
@@ -1,16 +1,37 @@
-// import { getFirestore, collection, getDocs } from "firebase/firestore";
-// import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+//import { useEffect } from "react";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmxCV8XAUCNRwY0bDJpKdN9zRUmtMTyBw",
+  authDomain: "proyectoreactch-29d90.firebaseapp.com",
+  databaseURL: "https://proyectoreactch-29d90-default-rtdb.firebaseio.com",
+  projectId: "proyectoreactch-29d90",
+  storageBucket: "proyectoreactch-29d90.appspot.com",
+  messagingSenderId: "946188183402",
+  appId: "1:946188183402:web:466093a53c63b64c60e713",
+};
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDmxCV8XAUCNRwY0bDJpKdN9zRUmtMTyBw",
-//   authDomain: "proyectoreactch-29d90.firebaseapp.com",
-//   databaseURL: "https://proyectoreactch-29d90-default-rtdb.firebaseio.com",
-//   projectId: "proyectoreactch-29d90",
-//   storageBucket: "proyectoreactch-29d90.appspot.com",
-//   messagingSenderId: "946188183402",
-//   appId: "1:946188183402:web:466093a53c63b64c60e713",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const DB = getFirestore(app);
+// Initialize Firebase
 // export const DB = getFirestore(app);
+// useEffect(() => {
+//   const colRef = collection(DB, "products");
+
+//   getDocs(colRef).then((snapshot) => {
+//     console.log(">> snapshot.docs");
+
+//     const products = snapshot.docs.map((rawDoc) => {
+//       return {
+//         id: rawDoc.id,
+//         ...rawDoc.data(),
+//       };
+//     });
+//     console.log(products);
+//   });
+// });

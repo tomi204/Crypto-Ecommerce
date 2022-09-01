57937a3e803dc7927b27233f8262a0805db44f8f
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-09-01 13:48:26 -0300

    linkedin added

--- src/components/lateralnav/NavL.jsx
+++ src/components/lateralnav/NavL.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./NavL.css";
-import { FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
+import { FaInstagram, FaTiktok, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { getAll } from "../Api/api";
 
 function NavL() {
   return (
     <ul className="navl">
       <li className="li-n">
-        <a href="">
+        <a href="https://www.instagram.com/tomi204_/">
           <FaInstagram></FaInstagram>
         </a>
-        <a >
+        <a href="https://twitter.com/tomiioliver">
           <FaTwitter></FaTwitter>
+        </a>
+        <a href="https://www.linkedin.com/in/tomi204/">
+          <FaLinkedin></FaLinkedin>
         </a>
       </li>
     </ul>

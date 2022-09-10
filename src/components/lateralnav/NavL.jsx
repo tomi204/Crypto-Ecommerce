67b652e319269832a3def87b14d1cb117484d4ec
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-09-10 01:27:55 -0300

    lateral navbar design mobile

--- src/components/lateralnav/NavL.jsx
+++ src/components/lateralnav/NavL.jsx
@@ -8,18 +8,20 @@ function NavL() {
   return (
     <ul className="navl">
       <li className="li-n">
-        <a href="https://www.instagram.com/tomi204_/">
-          <FaInstagram></FaInstagram>
-        </a>
-        <a href="https://twitter.com/tomiioliver">
-          <FaTwitter></FaTwitter>
-        </a>
-        <a href="https://www.linkedin.com/in/tomi204/">
-          <FaLinkedin></FaLinkedin>
-        </a>
-        <a href="https://github.com/tomi204">
-          <FaGithub />
-        </a>
+        <div className="a-navl">
+          <a href="https://www.instagram.com/tomi204_/">
+            <FaInstagram></FaInstagram>
+          </a>
+          <a href="https://twitter.com/tomiioliver">
+            <FaTwitter></FaTwitter>
+          </a>
+          <a href="https://www.linkedin.com/in/tomi204/">
+            <FaLinkedin></FaLinkedin>
+          </a>
+          <a href="https://github.com/tomi204">
+            <FaGithub />
+          </a>
+        </div>
       </li>
     </ul>
   );

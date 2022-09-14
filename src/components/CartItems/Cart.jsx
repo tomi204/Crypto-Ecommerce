35c38bbe5bf3b90d74aac7a4e4dea8bf78d4f170
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-09-14 14:59:13 -0300

    css adapt y refresh page ordenado

--- src/components/CartItems/Cart.jsx
+++ src/components/CartItems/Cart.jsx
@@ -46,12 +46,13 @@ const Cart = () => {
     total: calcTotal()
   }
   const refreshPage = () => {
-    window.location.reload();
+    window.location.reload(false);
   }
   const createOrder = () => {
     const db = getFirestore();
     const ordersC = collection(db, "ordersEcommerce");
-    addDoc(ordersC, order).then(({ id }) => console.log(id)).finally(() => refreshPage())
+    addDoc(ordersC, order).then(({ id }) => console.log(id))
+    refreshPage()
   }
 
 
@@ -79,21 +80,26 @@ const Cart = () => {
                   <h2 className="contador">{item.qty}</h2>
                   <h2>${item.qty * item.cover}</h2>
                   <button className="eliminar" onClick={() => deleteQty(item.id)}>eliminar producto</button>
-                  <h1>Terminar compra</h1>
+
 
                 </div>
               ))}<h3>total: ${calcTotal(totalQtty)}</h3>
               <br />
-              <div>
+              <div className="formulario">
+                <h1>Terminar compra</h1>
                 <label>Nombre y Apellido</label>
                 <input type='text' placeholder='Nombre y apellido' required />
 
                 <label>Email</label>
                 <input type='email' name='mail' placeholder='Email' required />
-
+                <br />
                 <label>Direccion</label>
                 <input type='text' placeholder='Direccion' />
-                <button className="btn-comprar" onClick={createOrder}>finalizar compra</button>
+                <br />
+                <br />
+                <br />
+                <button className="btn-comprar" onClick={createOrder}> terminar compra</button>
+
               </div>
             </>
 

8123ba7f847e35e20f859776bbdde651b1cf485c
tomas oliver <tomi204@outlook.com>
2022-09-30 00:24:40 -0300

    pago en eth terminado

--- src/components/Metamask/MetaBtn.jsx
+++ src/components/Metamask/MetaBtn.jsx
@@ -1,57 +1,65 @@
-import React from 'react'
-import { useState } from 'react'
-import "./MetaBtn.css"
-import { useContext } from 'react';
+import React from "react";
+import { useState } from "react";
+import "./MetaBtn.css";
+import { useContext } from "react";
 import { DataContext } from "../context";
-import Web3 from 'web3';
+import Web3 from "web3";
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 export default function MetaBtn() {
- const { calcTotal } = useContext(DataContext);
-  const [buttonText, setButtonText] = useState('Connect wallet')
-  const [account, setAccount] = useState(null)
+  const { calcTotal } = useContext(DataContext);
+  const [buttonText, setButtonText] = useState("Connect wallet");
+  const [account, setAccount] = useState(null);
   const connectWallet = () => {
     if (window.ethereum && window.ethereum.isMetaMask) {
-      window.ethereum.request({ method: 'eth_requestAccounts' })
-        .then(result => {
-      setAccount(result[0])
-          setButtonText(null)
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          setAccount(result[0]);
+          setButtonText(null);
         })
-        .catch(error => {
-          setButtonText("error al conectar")
-        })
+        .catch((error) => {
+          setButtonText("error al conectar");
+        });
     } else {
       // no instalado
-      setButtonText('you need to install one wallet')
+      setButtonText("you need to install one wallet");
     }
-  }
- 
+  };
+
   const sendTransaction = () => {
     if (window.ethereum && window.ethereum.isMetaMask) {
-      window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [{
-          from: account,
-          to: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-          value: web3.utils.toHex(web3.utils.toWei(calcTotal().toString(), 'tether')),
-          gas: '0x5208',
-        }],
-      })
-        .then(result => {
-          setButtonText('Transaction sent')
+      window.ethereum
+        .request({
+          method: "eth_sendTransaction",
+          params: [
+            {
+              from: account,
+              to: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+              value: web3.utils.toHex(
+                web3.utils.toWei(calcTotal().toString(), "ether")
+              ),
+              gas: "0x5208",
+            },
+          ],
         })
-        .catch(error => {
-          setButtonText('Transaction error')
+        .then((result) => {
+          setButtonText("Transaction sent");
         })
-    }}
-      
-
-
+        .catch((error) => {
+          setButtonText("Transaction error");
+        });
+    }
+  };
 
   return (
     <div className="container">
-    <button className='btn-M' onClick={connectWallet}>{buttonText}{account}</button>
-     <button className='btn-M' onClick={sendTransaction}>comprar</button>
+      <button className="btn-M" onClick={connectWallet}>
+        {buttonText}
+        {account}
+      </button>
+      <button className="btn-M" onClick={sendTransaction}>
+        comprar
+      </button>
     </div>
-  )
+  );
 }
-
--- src/components/productos/productos.jsx
+++ src/components/productos/productos.jsx
@@ -32,7 +32,7 @@ export default function Productos() {
             <div className="div-product" key={item.id} >
               <img src={item.cover} alt="" className="img-products" />
               <h2>{item.tittle}</h2>
-              <h4 className="precio">${item.price / getEthPrice(0)}</h4>
+              <h4 className="precio">${item.price}</h4>
               <button className="addC-P" onClick={() => createProduct(item.id, item.tittle, item.price, item.cover, item.stock, item.desc)}> + </button>
               <Link to={`/category/${item.id}`} className="link-detalles">
                 <button className="boton-detalles">Ver Detalles</button>

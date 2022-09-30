db7dafe88f023911f53c3266ae1094f40f251049
tomas oliver <tomi204@outlook.com>
2022-09-30 01:15:13 -0300

    cambios footer

--- src/components/productos/ProductDetails.jsx
+++ src/components/productos/ProductDetails.jsx
@@ -6,8 +6,9 @@ import { GetAll } from "../Api/api";
 import { useContext } from "react";
 import { DataContext } from "../context";
 const ProductDetails = () => {
-  // datacontext from context and get data from api 
-  const { blogs, setBlogs, addToCart, decreaseQty, CartItem } = useContext(DataContext);
+  // datacontext from context and get data from api
+  const { blogs, setBlogs, addToCart, decreaseQty, CartItem } =
+    useContext(DataContext);
   const { id } = useParams();
   // const productsId = GetAll();
   function createProduct(id, tittle, cover, desc, price, stock, category) {
@@ -18,10 +19,9 @@ const ProductDetails = () => {
       desc,
       price,
       stock,
-      category
-    }
-    addToCart(product)
-
+      category,
+    };
+    addToCart(product);
   }
   function decreaseProduct(id, tittle, cover, desc, price, stock, category) {
     let product = {
@@ -31,39 +31,74 @@ const ProductDetails = () => {
       desc,
       price,
       stock,
-      category
-    }
-    decreaseQty(product)
-
+      category,
+    };
+    decreaseQty(product);
   }
 
   return (
     <div className="productos">
-      {blogs && blogs.filter(item => item.id === Number(id)).map((item) => {
-        return (
-          <div className="list-item" key={item.id}>
-            <br></br>
-            <img src={item.cover} alt="" className="img-details" />
-            <h1>{item.title}</h1>
-            <h3 className="desc-id">{item.desc}</h3>
-            <br></br>
-            <h2> ${item.price}</h2>
-            <h2>Hay {item.stock} en stock</h2>
-            <br />
-            <button className="addCart-D" onClick={() => createProduct(item.id, item.tittle, item.price, item.cover, item.stock, item.desc, item.category)}> + </button>
-            <button className="btnN" onClick={() => decreaseProduct(item.id, item.tittle, item.price, item.cover, item.category, item.stock, item.desc)}>-</button>
+      {blogs &&
+        blogs
+          .filter((item) => item.id === Number(id))
+          .map((item) => {
+            return (
+              <div className="list-item" key={item.id}>
+                <br></br>
+                <img src={item.cover} alt="" className="img-details" />
+                <h1>{item.title}</h1>
+                <h3 className="desc-id">{item.desc}</h3>
+                <br></br>
+                <h2> {item.price} ethers</h2>
+                <h2>Hay {item.stock} en stock</h2>
+                <br />
+                <button
+                  className="addCart-D"
+                  onClick={() =>
+                    createProduct(
+                      item.id,
+                      item.tittle,
+                      item.price,
+                      item.cover,
+                      item.stock,
+                      item.desc,
+                      item.category
+                    )
+                  }
+                >
+                  {" "}
+                  +{" "}
+                </button>
+                <button
+                  className="btnN"
+                  onClick={() =>
+                    decreaseProduct(
+                      item.id,
+                      item.tittle,
+                      item.price,
+                      item.cover,
+                      item.category,
+                      item.stock,
+                      item.desc
+                    )
+                  }
+                >
+                  -
+                </button>
 
-            {/* // {goToCart ? */}
-            <Link to={"/Cart"} className="link-cart">Ir al carrito</Link>
-            {/* //  :
+                {/* // {goToCart ? */}
+                <Link to={"/Cart"} className="link-cart">
+                  Ir al carrito
+                </Link>
+                {/* //  :
          //   <Contador onAdd={onAdd} /> */}
 
-            {/* } */}
-          </div>
-        )
-
-      })}</div>
-  )
-}
+                {/* } */}
+              </div>
+            );
+          })}
+    </div>
+  );
+};
 
 export default ProductDetails;

075d1d5f3b1c9bac962d082cfa991025c65fa3e9
tomas oliver <tomi204@outlook.com>
2022-09-30 21:25:58 -0300

    mobile terminado

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { FaInstagram, FaLinkedin, FaTelegram, FaTwitter } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 import "./Footer.css";
 export const Footer = () => {
   return (
     <footer className="footer-main">
-      <h3>Tomi204© 2022</h3>
+      <h3 className="derechos">Tomi204© 2022</h3>
       <div className="footer-container">
-        <a href="">
+        <a href="https://twitter.com/tomiioliver">
           <FaTwitter />
         </a>
-        <a href="">
+        <a href="https://www.instagram.com/tomi204_/">
           <FaInstagram />
         </a>
-        <a href="">
+        <a href="https://www.linkedin.com/in/tomi204/">
           <FaLinkedin />
         </a>
 
-        <a href="">
-          <FaTelegram />
+        <a href="https://github.com/tomi204">
+          <FaGithub />
         </a>
       </div>
     </footer>
